76e71d4ba804e1a4b611bf832b6b79af65a7a7b6
kimichen2907@gmail.com <kimichen2907@gmail.com>
2023-07-05 22:42:46 +0800

    test

--- ctk.py
+++ ctk.py
@@ -1,3 +1,22 @@
 import customtkinter as ctk
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 window=ctk.CTk()
+window.configure()
+window.title("test")
+window.geometry('600x400')
+label=ctk.CTkLabel(window,
+                   text=".w.",
+                   fg_color="blue",
+                   text_color="white",
+                   corner_radius=10)
+label.pack()
+
+button=ctk.CTkButton(window,text="按鈕"
+                     ,corner_radius=10
+                     ,fg_color="#FF0"
+                     ,text_color="#000"
+                     ,hover_color="AA0")
+button.pack()
+
 window.mainloop()
