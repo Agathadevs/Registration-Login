edcbd9f6eacce015eab3602c610d43dcd52355ed
kimichen2907@gmail.com <kimichen2907@gmail.com>
2023-07-11 15:02:51 +0800

    te

--- GUI.py
+++ GUI.py
@@ -23,15 +23,16 @@ class ToplevelSignupWindow(ctk.CTkToplevel):
                     data = json.load(f)
                 data["User"].update(ndata)
                 with open("info.json","w") as f:
-                    json.dump(data,f)        
+                    json.dump(data,f)  
+                self.label=ctk.CTkLabel(self,text=">>You have successfully created an account<<").pack()  
             else:
-                 pass
+                 self.label=ctk.CTkLabel(self,text=">>You failed to create an account<<").pack() 
              
 class ToplevelLogInWindow(ctk.CTkToplevel):
        def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.geometry("500x300")
-        self.label=ctk.CTkLabel(self,text="ew")
+        self.label=ctk.CTkLabel(self,text=":))")
         self.label.pack()
 
 
@@ -40,12 +41,34 @@ class App(ctk.CTk):
         super().__init__()
         self.title("my app")
         self.geometry("500x300")
-        self.entry=ctk.CTkEntry(self,placeholder_text="example@gmail.com",width=200).pack(padx=10,pady=15)
-        self.entry2=ctk.CTkEntry(self,placeholder_text="passward",width=200).pack(padx=10,pady=15)
+
+        self.entry=ctk.CTkEntry(self,placeholder_text="example@gmail.com",width=200)
+        self.entry.pack(padx=10,pady=15)
+        self.entry2=ctk.CTkEntry(self,placeholder_text="passward",width=200)
+        self.entry2.pack(padx=10,pady=15)
+        self.label=ctk.CTkLabel(self)
+        self.label.pack()
+        
         self.btn1=ctk.CTkButton(self,text="Log in",corner_radius=10,command=self.log_in).pack(padx=10,pady=10)
         self.btn2=ctk.CTkButton(self,text="Sign up",corner_radius=10,command=self.open_toplevel).pack(padx=10,pady=10)
+        
+        
     def log_in(self):
-            ToplevelLogInWindow(self)
+            with open("info.json","r") as f:
+             data=json.load(f)
+            acc=self.entry.get()
+            ps=self.entry2.get()
+            if acc in data["User"].keys():
+                if data["User"][acc]==ps:
+                    ToplevelLogInWindow(self)
+                else:
+                    self.label=ctk.CTkLabel(self,text=">>Incorrect password<<",text_color="white")
+                    self.label.pack()
+            else:
+                self.label=ctk.CTkLabel(self,text=">>Incorrect password or Incorrect account <<",text_color="white")
+                self.label.pack()
+
+
     def open_toplevel(self):
             ToplevelSignupWindow(self)
 
